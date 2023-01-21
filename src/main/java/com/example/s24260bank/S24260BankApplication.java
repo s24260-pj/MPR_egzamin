df17106398d96593a9b2fc19e38b0076a73adda0
s24260 <s24260@pjwstk.edu.pl>
2023-01-21 14:54:09 +0100

    Initial Commit.

---
+++ src/main/java/com/example/s24260bank/S24260BankApplication.java
@@ -0,0 +1,13 @@
+package com.example.s24260bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S24260BankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S24260BankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/s24260bank/S24260BankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.s24260bank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S24260BankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
